5d733fe2b4c09366d12da2dbbddde160ef56246a
demoore <demoore@homeaway.com>
2020-02-03 15:16:53 -0600

    Add item summary grid component

--- pages/highVolume.tsx
+++ pages/highVolume.tsx
@@ -2,32 +2,19 @@ import React, { ReactElement } from 'react';
 import { NextPage } from 'next';
 import usePriceSummary from '../hooks/usePriceSummary';
 import { OsBuddyItemSummary } from '../types/osbuddy';
-import {highVolumeFilter} from '../filters';
+import { highVolumeFilter as filter } from '../filters';
 
 import '../styles.scss';
+import ItemSummaryGrid from '../components/itemSummaryGrid';
 
 function profitMarginSort(itemA: OsBuddyItemSummary, itemB: OsBuddyItemSummary): number {
     return (itemB.buy_average - itemB.sell_average) - (itemA.buy_average - itemA.sell_average);
 }
 
-function itemRow(item: OsBuddyItemSummary): ReactElement {
-    return <tr key={item.id}>
-        <td>{item.id}</td>
-        <td>{item.name}</td>
-        <td>{item.buy_average}</td>
-        <td>{item.sell_average}</td>
-        <td>{item.buy_average - item.sell_average}</td>
-        <td>{`${((item.buy_average - item.sell_average)/item.sell_average*100).toFixed(2)}%`}</td>
-        <td>{item.buy_quantity}</td>
-        <td>{item.sell_quantity}</td>
-        <td>{(item.sell_quantity / item.buy_quantity).toFixed(2)}</td>
-    </tr>
-}
-
 const HighVolume: NextPage = function () {
-    const {summary} = usePriceSummary();
+    const { summary } = usePriceSummary();
     const items = summary?.getItems()
-        ?.filter(highVolumeFilter)
+        ?.filter(filter)
         ?.sort(profitMarginSort)
         ?? [];
 
@@ -36,26 +23,7 @@ const HighVolume: NextPage = function () {
             <h1 className="title">Items</h1>
             <h2 className="subtitle">High Volume</h2>
 
-            <div className="container">
-                <table className="table is-fullwidth is-striped is-hoverable">
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Buy Price</th>
-                            <th>Sell Price</th>
-                            <th>Profit</th>
-                            <th>ROI</th>
-                            <th>Buy Quantity</th>
-                            <th>Sell Quantity</th>
-                            <th>Buy/Sell Ratio</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {items.map(itemRow)}
-                    </tbody>
-                </table>
-            </div>
+            <ItemSummaryGrid items={items} />
         </div>
     );
 }
