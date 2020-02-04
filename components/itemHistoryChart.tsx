5cc6e547a9cb589dccb86b1b6f381bddfe461080
Oelderoth <GMZues@gmail.com>
2020-02-03 23:40:14 -0600

    Add quantity charts

--- components/itemHistoryChart.tsx
+++ components/itemHistoryChart.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement, useState } from 'react';
 import useItemPriceHistory from '../hooks/useItemPriceHistory';
 import { OsBuddyItemHistoryEntry } from '../types/osbuddy';
-import { Line } from 'react-chartjs-2';
+import { Line, Bar } from 'react-chartjs-2';
 import classNames from 'classnames';
 
 import '../styles.scss';
@@ -39,7 +39,7 @@ const chartOptions = {
     }
 }
 
-function buildGraphDataset(history: OsBuddyItemHistoryEntry[]) {
+function buildGraphDatasets(history: OsBuddyItemHistoryEntry[]) {
     const defaultStyle = {
         fill: false,
         lineTension: 0.25,
@@ -48,22 +48,43 @@ function buildGraphDataset(history: OsBuddyItemHistoryEntry[]) {
         spanGaps: true
     };
 
-    return {
+    const priceDataset = {
         datasets: [
             {
                 ...defaultStyle,
                 label: 'Sell Price',
                 borderColor: 'rgba(240, 150, 113,1)',
-                data: history?.filter(entry => entry.buyingPrice > 0).map(entry => ({ t: new Date(entry.ts), y: entry.buyingPrice })) ?? []
+                data: history?.filter(entry => entry.buyingPrice > 0).map(entry => ({ t: new Date(entry.ts), y: entry.buyingPrice })).slice(0,32) ?? []
             },
             {
                 ...defaultStyle,
                 label: 'Offer Price',
                 borderColor: 'rgba(113, 151, 240,1)',
-                data: history?.filter(entry => entry.sellingPrice > 0).map(entry => ({ t: new Date(entry.ts), y: entry.sellingPrice })) ?? []
+                data: history?.filter(entry => entry.sellingPrice > 0).map(entry => ({ t: new Date(entry.ts), y: entry.sellingPrice })).slice(0,32) ?? []
+            }
+        ]
+    };
+
+    const quantityDataset = {
+        datasets: [
+            {
+                ...defaultStyle,
+                label: 'Sell Quantity',
+                borderColor: 'rgba(240, 150, 113,1)',
+                backgroundColor: 'rgba(240, 150, 113,0.4)',
+                data: history?.filter(entry => entry.buyingPrice > 0).map(entry => ({ t: new Date(entry.ts), y: entry.sellingQuantity })).slice(0,32) ?? []
+            },
+            {
+                ...defaultStyle,
+                label: 'Buy Quantity',
+                borderColor: 'rgba(113, 151, 240,1)',
+                backgroundColor: 'rgba(113, 151, 240, 0.4)',
+                data: history?.filter(entry => entry.sellingPrice > 0).map(entry => ({ t: new Date(entry.ts), y: entry.buyingQuantity })).slice(0,32) ?? []
             }
         ]
     }
+
+    return [priceDataset, quantityDataset];
 }
 
 const TimeSpanButton = (props: TimeSpanButtonProps): ReactElement => {
@@ -80,9 +101,9 @@ const TimeSpanButton = (props: TimeSpanButtonProps): ReactElement => {
 }
 
 const ItemHistoryChart = function (props: ItemHistoryChartProps): ReactElement {
-    const [timeSpan, setTimeSpan] = useState(90);
+    const [timeSpan, setTimeSpan] = useState(30);
     const { history } = useItemPriceHistory(props.id, timeSpan);
-    const dataset = buildGraphDataset(history)
+    const [priceDataset, quantityDataset] = buildGraphDatasets(history)
 
     return (
         <div>
@@ -90,9 +111,10 @@ const ItemHistoryChart = function (props: ItemHistoryChartProps): ReactElement {
                 <TimeSpanButton label="Quarter" timeSpan={4320} setTimeSpan={setTimeSpan} currentTimeSpan={timeSpan}/>
                 <TimeSpanButton label="Month" timeSpan={1440} setTimeSpan={setTimeSpan} currentTimeSpan={timeSpan}/>
                 <TimeSpanButton label="Week" timeSpan={180} setTimeSpan={setTimeSpan} currentTimeSpan={timeSpan}/>
-                <TimeSpanButton label="Day" timeSpan={90} setTimeSpan={setTimeSpan} currentTimeSpan={timeSpan}/>
+                <TimeSpanButton label="Day" timeSpan={30} setTimeSpan={setTimeSpan} currentTimeSpan={timeSpan}/>
             </div>
-            <Line data={dataset} options={chartOptions} />
+            <Line height={50} data={priceDataset} options={chartOptions} />
+            <Bar height={50}data={quantityDataset} options={chartOptions} />
         </div>
     );
 }
