b6eefef733b161683aef57a096a447ee9094cf78
Oelderoth <GMZues@gmail.com>
2020-02-15 15:57:03 -0600

    Add links

--- components/header.tsx
+++ components/header.tsx
@@ -38,33 +38,36 @@ export default () => {
             })}>
                 <div className="navbar-start">
                     <Link href="/favoriteItems"><a className="navbar-item">Favorites</a></Link>
-                    <Link href="/suggestedItems"><a className="navbar-item">Suggested Items</a></Link>
+                    <Link href="/transactions"><a className="navbar-item">Transaction Log</a></Link>
                     <div className="navbar-item has-dropdown is-hoverable">
-                        <a className="navbar-link">Others</a>
+                    <Link href="/suggestedItems"><a className="navbar-link">Flip Finder</a></Link>
                         <div className="navbar-dropdown">
+                            <Link href="/suggestedItems"><a className="navbar-item">Suggested Items</a></Link>
+                            <Link href="/allItems"><a className="navbar-item">All Items</a></Link>
                             <Link href="/highVolume"><a className="navbar-item">High Volume</a></Link>
                             <Link href="/highestMargin"><a className="navbar-item">High Margin</a></Link>
+                            <div className="navbar-divider" />
+                            <div className="navbar-item has-dropdown is-hoverable">
+                                <a className="navbar-link">Item Categories</a>
+                                <div className="navbar-dropdown">
+                                    <Link href="/category/runes"><a className="navbar-item">Runes</a></Link>
+                                    <Link href="/category/construction"><a className="navbar-item">Construction</a></Link>
+                                    <Link href="/category/herblore"><a className="navbar-item">Crafting</a></Link>
+                                    <Link href="/category/farming"><a className="navbar-item">Farming</a></Link>
+                                    <Link href="/category/fletching"><a className="navbar-item">Fletching</a></Link>
+                                    <Link href="/category/food"><a className="navbar-item">Food</a></Link>
+                                    <Link href="/category/herblore"><a className="navbar-item">Herblore</a></Link>
+                                    <Link href="/category/implings"><a className="navbar-item">Implings</a></Link>
+                                    <Link href="/category/logs"><a className="navbar-item">Logs</a></Link>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="navbar-item has-dropdown is-hoverable">
-                        <a className="navbar-link">Tags</a>
+                        <a className="navbar-link">Money Making</a>
                         <div className="navbar-dropdown">
-                            <a className="navbar-item">Before</a>
-                            <div className="navbar-item has-dropdown is-hoverable">
-                                <a className="navbar-link">Herblore</a>
-                                <div className="navbar-dropdown">
-                                    <Link href="/highVolume"><a className="navbar-item">Unfinished Potions</a></Link>
-                                    <Link href="/highestMargin"><a className="navbar-item">Secondaries</a></Link>
-                                    <div className="navbar-item has-dropdown is-hoverable">
-                                        <a className="navbar-link">Nested</a>
-                                        <div className="navbar-dropdown">
-                                            <Link href="/highVolume"><a className="navbar-item"> Potions</a></Link>
-                                            <Link href="/highestMargin"><a className="navbar-item">Secondaries</a></Link>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <a className="navbar-item">After</a>
+                            <Link href="/decanting"><a className="navbar-item">Decanting</a></Link>
+                            <Link href="/itemSets"><a className="navbar-item">Construction</a></Link>
                         </div>
                     </div>
                 </div>
