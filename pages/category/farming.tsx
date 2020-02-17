770b2bbada353f3dbc2e5e1b161ebbbe43480391
Oelderoth <GMZues@gmail.com>
2020-02-17 14:10:14 -0600

    Add basic item categories pages (#12)
    * Add links
    
    * Add basic filters for item category pages

---
+++ pages/category/farming.tsx
@@ -0,0 +1,49 @@
+import React, { ReactElement } from 'react';
+import { NextPage } from 'next';
+import usePriceSummary from '../../hooks/usePriceSummary';
+import { OsBuddyItemSummary } from '../../types/osbuddy';
+
+import '../../styles.scss';
+import ItemSummaryGrid from '../../components/itemSummaryGrid';
+
+function profitMarginSort(itemA: OsBuddyItemSummary, itemB: OsBuddyItemSummary): number {
+    return (itemB.buy_average - itemB.sell_average) - (itemA.buy_average - itemA.sell_average);
+}
+
+const itemWhitelist = new Set<String|RegExp|number>([
+    /\s+seed$/ig
+]);
+
+const filter = (item: OsBuddyItemSummary): boolean => {
+    for (let val of itemWhitelist.values()) {
+        if (val instanceof RegExp) {
+            if (val.test(item.name)) {
+                return true;
+            }
+        } else if (typeof val === 'number') {
+            if (val === item.id) {
+                return true;
+            }
+        }
+    }
+    return itemWhitelist.has(item.name.toLowerCase());
+}
+
+const RunesCategoryPage: NextPage = function () {
+    const { summary } = usePriceSummary();
+    const items = summary?.getItems()
+        ?.filter(filter)
+        ?.sort(profitMarginSort)
+        ?? [];
+
+    return (
+        <div className="section">
+            <h1 className="title">Items</h1>
+            <h2 className="subtitle">Category - Runes</h2>
+
+            <ItemSummaryGrid items={items} />
+        </div>
+    );
+}
+
+export default RunesCategoryPage;
